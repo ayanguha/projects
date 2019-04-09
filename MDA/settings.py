17acbc64feae33bcae6bfda34e1431d5d5c8f581
Ayan Guha <Ayan.Guha@aemo.com.au>
2019-04-09 21:26:58 +1000

    Local SQL DB added

--- MDA/settings.py
+++ MDA/settings.py
@@ -1,4 +1,4 @@
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///dbtst.sqlite'
-
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///dbtst.sqlite'
+SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://localhost\SQLEXPRESS/mda?driver=SQL+Server+Native+Client+11.0'
 FLASK_DEBUG=False
